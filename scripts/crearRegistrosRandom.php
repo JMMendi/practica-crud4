457ff41142d8787b5316669a5839bc6df657e7b5
JMMendi <mendizabaly@gmail.com>
2024-11-20 12:48:56 +0100

    Creados articulo.php, nuevo.php y añadido funcionalidades a ambas

--- scripts/crearRegistrosRandom.php
+++ scripts/crearRegistrosRandom.php
@@ -1,8 +1,24 @@
 <?php
 
 use App\Db\Categoria;
+use App\Db\Articulo;
 
-require __DIR__."/../vendor/autoload.php";
+$cantidad = 0;
 
-Categoria::crearRegistrosRandom();
+do {
+    $cantidad = readline("Indique el número de artículos a crear: (Escriba 0 para salir) ");
+    if ($cantidad == 0) {
+        echo "Saliendo.";
+        break;
+    }
+    if ($cantidad < 5 || $cantidad > 50) {
+        echo "Error, tiene que crear entre 5 y 30 artículos. Recuerde, 0 para salir.";
+    }
+} while (($cantidad < 5 || $cantidad > 50) && ($salir = false));
 
+if($cantidad != 0) {
+    require __DIR__."/../vendor/autoload.php";
+    Categoria::crearRegistrosRandom();
+    Articulo::crearArticulosRandom($cantidad);
+    echo "Se han creado $cantidad artículos.".PHP_EOL;
+}
